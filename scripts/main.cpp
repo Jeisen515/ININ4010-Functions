8a30c4500e264b59495535b38e7263d564baf512
Jeisen515 <jeisen.soler@upr.edu>
2022-08-24 15:31:35 -0400

    Code Worked

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -8,16 +8,16 @@
 
 using namespace std;
 
-float geometric_mean(vector<float> dataVector); // Calculates the geometric mean of a vector of integers
-float harmonic_mean(vector<float> dataVector); // Calculates the harmonic mean of a vector of integers
-float arithmetic_mean(vector<float> dataVector); // Calculates the arithmetic mean of a vector of integers
-float medianFunction(vector<float> dataVector); // Calculates the median of a vector of integers
-float mode(vector<float> dataVector); // Calculates the mode of a vector of integers
-float standard_deviation(vector<float> dataVector); // Calculates the standard deviation of a vector of integers
-float variance(vector<float> dataVector); // Calculates the variance of a vector of integers
-float range(vector<float> dataVector); // Calculates the range of a vector of integers
-float midrange(vector<float> dataVector); // Calculates the midrange of a vector of integers
-float midrange_absolute(vector<float> dataVector); // Calculates the midrange absolute of a vector of integers
+float geometric_mean(vector<float> sortedVector); // Calculates the geometric mean of a vector of integers
+float harmonic_mean(vector<float> sortedVector); // Calculates the harmonic mean of a vector of integers
+float arithmetic_mean(vector<float> sortedVector); // Calculates the arithmetic mean of a vector of integers
+float medianFunction(vector<float> sortedVector); // Calculates the median of a vector of integers
+float mode(vector<float> sortedVector); // Calculates the mode of a vector of integers
+float standard_deviation(vector<float> sortedVector); // Calculates the standard deviation of a vector of integers
+float variance(vector<float> sortedVector); // Calculates the variance of a vector of integers
+float range(vector<float> sortedVector); // Calculates the range of a vector of integers
+float midrange(vector<float> sortedVector); // Calculates the midrange of a vector of integers
+float midrange_absolute(vector<float> sortedVector); // Calculates the midrange absolute of a vector of integers
 
 vector<float> bubbleSort(vector<float> dataVector); // Sorts a vector of integers using bubble sort
 
@@ -32,57 +32,41 @@ int main()
     cout << "Enter the elements of the vector: ";
 
     for (int i = 0; i < dataSize; i++)
-    {
+    {   
         int x;
         cin >> x;
         dataVector.push_back(x);
     }
 
-    cout << "The geometric mean of the vector is: " << geometric_mean(dataVector) << endl;
-    cout << "The harmonic mean of the vector is: " << harmonic_mean(dataVector) << endl;
-    cout << "The arithmetic mean of the vector is: " << arithmetic_mean(dataVector) << endl;
-    cout << "The median of the vector is: " << medianFunction(dataVector) << endl;
-    cout << "The mode of the vector is: " << mode(dataVector) << endl;
-    cout << "The standard deviation of the vector is: " << standard_deviation(dataVector) << endl;
-    cout << "The variance of the vector is: " << variance(dataVector) << endl;
-    cout << "The range of the vector is: " << range(dataVector) << endl;
-    cout << "The midrange of the vector is: " << midrange(dataVector) << endl;
-    cout << "The midrange absolute of the vector is: " << midrange_absolute(dataVector) << endl;
+    vector<float> sortedVector = bubbleSort(dataVector); // Sorts the vector
+
+    cout << "The geometric mean of the vector is: " << geometric_mean(sortedVector) << endl;
+    cout << "The harmonic mean of the vector is: " << harmonic_mean(sortedVector) << endl;
+    cout << "The arithmetic mean of the vector is: " << arithmetic_mean(sortedVector) << endl;
+    cout << "The median of the vector is: " << medianFunction(sortedVector) << endl;
+    cout << "The mode of the vector is: " << mode(sortedVector) << endl;
+    cout << "The standard deviation of the vector is: " << standard_deviation(sortedVector) << endl;
+    cout << "The variance of the vector is: " << variance(sortedVector) << endl;
+    cout << "The range of the vector is: " << range(sortedVector) << endl;
+    cout << "The midrange of the vector is: " << midrange(sortedVector) << endl;
+    cout << "The midrange absolute of the vector is: " << midrange_absolute(sortedVector) << endl;
 
     return 0;
-
-vector<float> bubbleSort(vector<float> dataVector)
-{
-    int size = dataVector.size();
-
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (dataVector[j] > dataVector[j + 1])
-            {
-                int temp = v[j];
-                dataVector[j] = dataVector[j + 1];
-                dataVector[j + 1] = temp;
-            }
-        }
-    }
-    return v;
 }
 
 float geometric_mean(vector<float> dataVector){ // Calculates the geometric mean of a vector of integers
     
-    float sum = 0;
-    for(int i = 0; i < v.size(); i++){
-        sum += dataVector[i];
+    float product = 1;
+    for(int i = 0; i < dataVector.size(); i++){
+        product *= dataVector[i];
     }
-    return pow(sum, 1/dataVector.size());
+    return pow(product, 1/dataVector.size()+1);
 }
 
 float harmonic_mean(vector<float> dataVector){ // Calculates the harmonic mean of a vector of integers
     
     float sum = 0;
-    for(int i = 0; i < v.size(); i++){
+    for(int i = 0; i < dataVector.size(); i++){
         sum += 1/dataVector[i];
     }
     return dataVector.size()/sum;
@@ -91,7 +75,7 @@ float harmonic_mean(vector<float> dataVector){ // Calculates the harmonic mean o
 float arithmetic_mean(vector<float> dataVector){ // Calculates the arithmetic mean of a vector of integers
     
     float sum = 0;
-    for(int i = 0; i < v.size(); i++){
+    for(int i = 0; i < dataVector.size(); i++){
         sum += dataVector[i];
     }
     return sum/dataVector.size();
@@ -192,4 +176,21 @@ float midrange_absolute(vector<float> dataVector){ // Calculates the midrange ab
     return abs(max - min)/2;
     }
 
+vector<float> bubbleSort(vector<float> dataVector)
+{
+    int size = dataVector.size();
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (dataVector[j] > dataVector[j + 1])
+            {
+                int temp = dataVector[j];
+                dataVector[j] = dataVector[j + 1];
+                dataVector[j + 1] = temp;
+            }
+        }
+    }
+    return dataVector;
 }
